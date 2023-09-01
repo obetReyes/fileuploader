fb3596035a51c6c93489073ad5d29ffcc12da0f9
obet <75959582+obetReyes@users.noreply.github.com>
2023-09-01 14:20:22 -0600

    Update files.ts

--- backend/src/files/files.ts
+++ backend/src/files/files.ts
@@ -11,7 +11,7 @@ export const FilesProvider = {
     return v2.config({
       cloud_name:process.env.CLOUD_NAME,  
     api_key: process.env.CLOUD_API_KEY,
-      api_secret:process.env.CLOUD_API_SECRET,
+      api_secret:'L5KInZPPfqlfpaN8O2Ja1gvjllE',
     });
   },
 };
